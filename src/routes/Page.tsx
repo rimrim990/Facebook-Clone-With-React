3f6406b960c7724c7d40bccccaecd53886ba7abb
lim990718@naver.com <lim990718@naver.com>
2021-07-11 13:02:12 +0900

    [07.11] Auth service update

--- src/routes/Page.tsx
+++ src/routes/Page.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import Home from "../components/Home";
 import Auth from "../components/Auth";
 import Navigation from "../components/Navigation";
+import { User } from "../components/App";
 
 interface AppProps {
   isLoggedIn: boolean;
+  userInfo: User | null;
 }
 
-const Page = ({ isLoggedIn }: AppProps) => {
-  if (isLoggedIn) {
-    return <Home />;
+const Page = ({ isLoggedIn, userInfo }: AppProps) => {
+  if (isLoggedIn && userInfo) {
+    return <Home userInfo={userInfo} />;
   } else {
     return (
       <>
